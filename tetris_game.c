34c6c7ef23fbb5d10eb1c61aaf35d52cbf19db3d
Alexei Humeniy <yo@ahumeniy.net>
2023-03-04 02:29:36 -0300

    Minor changes from unleashed

--- tetris_game.c
+++ tetris_game.c
@@ -2,10 +2,6 @@
 #include <gui/gui.h>
 #include <input/input.h>
 #include <stdlib.h>
-// #include <stdbool.h>
-// #include <string.h>
-// #include <furi_hal_resources.h>
-// #include <furi_hal_gpio.h>
 #include <dolphin/dolphin.h>
 
 #define BORDER_OFFSET 1
@@ -15,6 +11,9 @@
 
 #define FIELD_WIDTH 11
 #define FIELD_HEIGHT 24
+
+#define MAX_FALL_SPEED 500
+#define MIN_FALL_SPEED 100
 
 typedef struct Point {
     // Also used for offset data, which is sometimes negative
@@ -169,7 +168,7 @@ static void tetris_game_input_callback(InputEvent* input_event, FuriMessageQueue
 static void tetris_game_init_state(TetrisState* tetris_state) {
     tetris_state->gameState = GameStatePlaying;
     tetris_state->numLines = 0;
-    tetris_state->fallSpeed = 500;
+    tetris_state->fallSpeed = MAX_FALL_SPEED;
     memset(tetris_state->playField, 0, sizeof(tetris_state->playField));
 
     memcpy(&tetris_state->currPiece, &shapes[rand() % 7], sizeof(tetris_state->currPiece));
@@ -303,6 +302,7 @@ static void
             tetris_game_render_curr_piece(tetris_state);
             uint8_t numLines = 0;
             uint8_t lines[] = {0, 0, 0, 0};
+            uint16_t nextFallSpeed;
 
             tetris_game_check_for_lines(tetris_state, lines, &numLines);
             if(numLines > 0) {
@@ -323,7 +323,11 @@ static void
                 uint16_t oldNumLines = tetris_state->numLines;
                 tetris_state->numLines += numLines;
                 if((oldNumLines / 10) % 10 != (tetris_state->numLines / 10) % 10) {
-                    tetris_state->fallSpeed -= 50;
+                    nextFallSpeed =
+                        tetris_state->fallSpeed - (100 / (tetris_state->numLines / 10));
+                    if(nextFallSpeed >= MIN_FALL_SPEED) {
+                        tetris_state->fallSpeed = nextFallSpeed;
+                    }
                 }
             }
 
